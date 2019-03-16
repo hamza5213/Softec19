86e59f299b1ccb10a56216ce6a01bb7b83c07b78
Abdul Hameed Azeemi <maxer232@gmail.com>
2019-03-16 17:29:58 +0500

    Push

--- app/src/main/java/softec19/com/softec19/Activity/MainActivity.java
+++ app/src/main/java/softec19/com/softec19/Activity/MainActivity.java
@@ -1,8 +1,10 @@
 package softec19.com.softec19.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import softec19.com.softec19.PlayVideo;
 import softec19.com.softec19.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        startActivity(new Intent(this, PlayVideo.class));
     }
 }
--- app/src/main/java/softec19/com/softec19/PlayVideo.java
+++ app/src/main/java/softec19/com/softec19/PlayVideo.java
@@ -1,26 +1,49 @@
 package softec19.com.softec19;
 
+import android.content.Context;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PlayVideo extends AppCompatActivity {
+import com.devbrackets.android.exomedia.listener.OnPreparedListener;
+import com.devbrackets.android.exomedia.ui.widget.VideoView;
+
+import cafe.adriel.androidstreamable.AndroidStreamable;
+import cafe.adriel.androidstreamable.callback.VideoCallback;
+import cafe.adriel.androidstreamable.model.Video;
+
+public class PlayVideo extends AppCompatActivity implements OnPreparedListener {
+
+    private VideoView videoView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
+        videoView = findViewById(R.id.video_view);
 
-        String shortCode = "ifjh";
+
+        String shortCode = "oapvz";
         AndroidStreamable.getVideo(shortCode, new VideoCallback() {
             @Override
-            public void onSuccess(int statusCode, MediaStore.Video video) {
-                // :D
+            public void onSuccess(int statusCode, Video video) {
+                videoView.setOnPreparedListener(PlayVideo.this);
+                String url = video.getFiles().get("mp4").getUrl();
+                url = "https:"+url.split(":",2)[1];
+                System.out.println(url);
+                videoView.setVideoURI(Uri.parse(url));
             }
             @Override
             public void onFailure(int statusCode, Throwable error) {
                 // :(
             }
         });
+    }
+    @Override
+    public void onPrepared() {
+        //Starts the video playback as soon as it is ready
+        videoView.start();
     }
 }
