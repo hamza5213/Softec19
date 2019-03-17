808b22268b8c093f716ec8429fc2622e33518002
Abdul Hameed Azeemi <maxer232@gmail.com>
2019-03-17 08:58:18 +0500

    Merge branch 'master' of https://github.com/hamza5213/Softec19
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/softec19/com/softec19/Activity/MainActivity.java
    #	app/src/main/java/softec19/com/softec19/Activity/UploadVideo.java

--- app/src/main/java/softec19/com/softec19/Adapters/ViewPagerAdapter.java
+++ app/src/main/java/softec19/com/softec19/Adapters/ViewPagerAdapter.java
@@ -1,4 +1,57 @@
 package softec19.com.softec19.Adapters;
 
-public class ViewPagerAdapter {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+import softec19.com.softec19.Interfaces.OnListFragmentInteractionListener;
+
+/**
+ * Created by hamza on 09-Jul-18.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter implements OnListFragmentInteractionListener {
+    Context context;
+    String[] tabTitles={"Basic User","Premium User"};
+    ArrayList<Fragment> fragments;
+
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        fragments=new ArrayList<>();
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+
+    public Fragment getFragment(int position)
+    {
+        return fragments.get(position);
+    }
+
+
+    @Override
+    public void onListFragmentInteraction(Bundle details, String action, boolean isFabClicked) {
+
+    }
 }
--- app/src/main/java/softec19/com/softec19/Utility/Categories.java
+++ app/src/main/java/softec19/com/softec19/Utility/Categories.java
@@ -1,4 +1,15 @@
 package softec19.com.softec19.Utility;
 
+import java.util.ArrayList;
+
 public class Categories {
+    public static ArrayList<String> getCategories(){
+        ArrayList<String> categories = new ArrayList<>();
+        categories.add("Horror");
+        categories.add("Sports");
+        categories.add("Drama");
+        categories.add("Politics");
+        return categories;
+
+    }
 }
